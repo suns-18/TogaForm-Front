e861223904011390b1f99e4026ba9e78951c89e4
NBDatsuya <2646809050@qq.com>
2023-06-07 10:34:20 +0800

    feat: navigation menu

---
+++ src/ts/navi-menu.ts
@@ -0,0 +1,28 @@
+export default [
+	{
+		title: '主页',
+		icon: 'mdi mdi-home-circle',
+		path: '/',
+	},{
+		title: '用户管理',
+		icon: 'mdi mdi-account-check-outline',
+		path: '/userManage',
+	}, {
+		title: '个人信息',
+		icon: 'mdi mdi-account',
+		path: '/personalInfo'
+	},
+	{
+		title: '使用帮助',
+		icon: 'mdi mdi-help',
+		path: '/help'
+	}, {
+		title: '关于本系统',
+		icon: 'mdi mdi-information',
+		path: '/about'
+	}, {
+		title: '退出登陆',
+		icon: 'mdi mdi-exit-run',
+		path: '/logout'
+	}
+]

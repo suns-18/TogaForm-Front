1c13a520d3f117907dd87b107c815466ad7cc355
NBDatsuya <2646809050@qq.com>
2023-06-07 10:33:48 +0800

    feat: Logout.vue

---
+++ src/pages/Logout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const initComponent = async ()=>{
+	let res = await axios.post(`/api/user/logout/`)
+	//Clear pinia
+	await router.push('/login')
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>

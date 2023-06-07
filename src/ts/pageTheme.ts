805ac37079a39ffde41f7edb2d8cd84133bb28bb
NBDatsuya <2646809050@qq.com>
2023-06-07 10:36:12 +0800

    feat: page theme utils

---
+++ src/ts/pageTheme.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import {ThemeDefinition} from 'vuetify';
+
+const pageTheme: ThemeDefinition = {
+	dark: false,
+	colors: {
+		royalblue: '#004D7C',
+		background: '#E0E8EC',
+		surface: '#F6F7F6',
+		error: '#B00020',
+		info: '#2196F3',
+		success: '#4CAF50',
+		warning: '#FB8C00',
+	}
+}
+
+export default pageTheme

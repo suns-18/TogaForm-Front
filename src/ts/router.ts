62a0e7d8ed1b1ab847f3780e435213369093b623
NBDatsuya <2646809050@qq.com>
2023-06-24 19:35:39 +0800

    feat: project manage

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -2,48 +2,74 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
-	{
-		path: '/',
-		component: () => import('../pages/Main.vue'),
-		children: [{
-			path: '/',
-			component: () => import('../pages/mainframe/Home.vue'),
-			alias: ['/main', '/index'],
-			meta: {
-				title: '主页 - TogaForm'
-			}
-		},{
-			path: '/userManage',
-			component: () => import('../pages/mainframe/UserManage.vue'),
-			meta: {
-				title: '用户管理 - TogaForm'
-			}
-		}]
-	}, {
-		path: '/login',
-		component: () => import('../pages/Login.vue'),
-		meta: {
-			title: '登陆 - TogaForm'
-		}
-	}, {
-		path: '/logout',
-		component: () => import('../pages/Logout.vue'),
-		meta: {
-			title: '登陆 - TogaForm'
-		}
-	}]
+    {
+        path: '/admin',
+        component: () => import('../pages/AdminMain.vue'),
+        children: [{
+            path: '/admin',
+            component: () => import('../pages/admin/Home.vue'),
+            meta: {
+                title: '后台主页 - TogaForm'
+            }
+        }, {
+            path: '/manage/user/closed',
+            component: () => import('../pages/admin/ClosedUserManage.vue'),
+            meta: {
+                title: '已关闭用户管理 - TogaForm'
+            }
+        }, {
+            path: '/manage/user',
+            component: () => import('../pages/admin/OpenUserManage.vue'),
+            meta: {
+                title: '正常用户管理 - TogaForm'
+            }
+        }, {
+            path: '/manage/project',
+            component: () => import('../pages/admin/ProjectManage.vue'),
+            meta: {
+                title: '项目管理 - TogaForm'
+            }
+        }]
+    }, {
+        path: '/login',
+        component: () => import('../pages/Login.vue'),
+        meta: {
+            title: '登陆 - TogaForm'
+        }
+    }, {
+        path: '/logout',
+        component: () => import('../pages/Logout.vue'),
+        meta: {
+            title: '登陆 - TogaForm'
+        }
+    }, {
+        path: '/quesnaire',
+        component: () => import('../pages/QuesnaireMain.vue'),
+        children: [{
+            path: '/quesnaire/:quesnaire_id',
+            component: () => import('../pages/quesnaire/TemplatePage.vue'),
+        },{
+            path: '/home',
+            component: () => import('../pages/quesnaire/Home.vue'),
+            meta: {
+                title: '主页 - TogaForm在线问卷系统'
+            }
+        }]
+    }
+]
 
 let router = createRouter({
-	history: createWebHistory(),
-	routes: routes
+    history: createWebHistory(),
+    routes: routes
 })
+
 // @ts-ignore
 router.beforeEach((to, from, next) => {
-	if (to.meta.title) {
-		// @ts-ignore
-		document.title = to.meta.title
-	}
-	next()
+    if (to.meta.title) {
+        // @ts-ignore
+        document.title = to.meta.title
+    }
+    next()
 
 })
 

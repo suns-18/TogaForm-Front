9e8b643da91fbfea429183063b8f52f32a60e338
NBDatsuya <2646809050@qq.com>
2023-06-24 19:36:02 +0800

    style: theme

--- src/ts/pageTheme.ts
+++ src/ts/pageTheme.ts
@@ -2,16 +2,17 @@
 import {ThemeDefinition} from 'vuetify';
 
 const pageTheme: ThemeDefinition = {
-	dark: false,
-	colors: {
-		royalblue: '#004D7C',
-		background: '#E0E8EC',
-		surface: '#F6F7F6',
-		error: '#B00020',
-		info: '#2196F3',
-		success: '#4CAF50',
-		warning: '#FB8C00',
-	}
+    dark: false,
+    colors: {
+        royalblue: '#004D7C',
+        background: '#E0E8EC',
+        'wjx-blue-1': '#0095FF',
+        surface: '#F6F7F6',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
 }
 
 export default pageTheme

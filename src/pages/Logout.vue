e11069a19696d9661b714ca82a6722b9d417c4e6
NBDatsuya <2646809050@qq.com>
2023-06-24 19:34:17 +0800

    fix: Logout failure

--- src/pages/Logout.vue
+++ src/pages/Logout.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-const initComponent = async ()=>{
-	let res = await axios.post(`/api/user/logout/`)
-	//Clear pinia
-	await router.push('/login')
+import axios from "../ts/axios";
+import router from "../ts/router";
+import {useUserStore} from "../ts/store/user-store.ts";
+import pinia from "../ts/store.ts";
+
+const userStore = useUserStore(pinia)
+const initComponent = async () => {
+    try {
+        let res = await axios.post(`/user/logout/`)
+    } finally {
+        userStore.user = {}
+        userStore.isLogin = false
+        await router.push('/login')
+    }
 }
+
+initComponent()
 </script>
 
 <template>

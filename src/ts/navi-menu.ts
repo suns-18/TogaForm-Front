0ae83aa8e7507428ee3bc630c937b7990cef59d0
NBDatsuya <2646809050@qq.com>
2023-06-24 19:35:33 +0800

    feat: project manage menu

--- src/ts/navi-menu.ts
+++ src/ts/navi-menu.ts
@@ -1,28 +1,40 @@
 export default [
-	{
-		title: '主页',
-		icon: 'mdi mdi-home-circle',
-		path: '/',
-	},{
-		title: '用户管理',
-		icon: 'mdi mdi-account-check-outline',
-		path: '/userManage',
-	}, {
-		title: '个人信息',
-		icon: 'mdi mdi-account',
-		path: '/personalInfo'
-	},
-	{
-		title: '使用帮助',
-		icon: 'mdi mdi-help',
-		path: '/help'
-	}, {
-		title: '关于本系统',
-		icon: 'mdi mdi-information',
-		path: '/about'
-	}, {
-		title: '退出登陆',
-		icon: 'mdi mdi-exit-run',
-		path: '/logout'
-	}
+    {
+        title: '主页',
+        icon: 'mdi mdi-home-circle',
+        path: '/admin',
+    }, {
+        title: '用户管理',
+        icon: 'mdi mdi-account-check-outline',
+        subItems: [{
+            title: '正常状态用户',
+            icon: 'mdi mdi-account-check-outline',
+            path: '/manage/user',
+        }, {
+            title: '已关闭用户',
+            icon: 'mdi mdi-account-check-outline',
+            path: '/manage/user/closed'
+        }]
+    }, {
+        title: '项目管理',
+        icon: 'mdi mdi-account-check-outline',
+        path: '/manage/project',
+    }, {
+        title: '个人信息',
+        icon: 'mdi mdi-account',
+        path: '/personalInfo'
+    },
+    {
+        title: '使用帮助',
+        icon: 'mdi mdi-help',
+        path: '/help'
+    }, {
+        title: '关于本系统',
+        icon: 'mdi mdi-information',
+        path: '/about'
+    }, {
+        title: '退出登陆',
+        icon: 'mdi mdi-exit-run',
+        path: '/logout'
+    }
 ]
